e53d9fef218df325e82db3d607205a7d4af76c2f
maleesha <139684247+Maleesha2001-malee@users.noreply.github.com>
2025-04-19 20:35:36 +0530

    create backend

---
+++ backend/finance-tracer-backend/src/main/java/com/com/financetracker/FinanceTracerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.com.financetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTracerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTracerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/finance-tracer-backend/src/test/java/com/com/financetracker/FinanceTracerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.financetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTracerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
