287b964d5e945a44609d2fa82ef50d6407dfb3c1
maleesha <139684247+Maleesha2001-malee@users.noreply.github.com>
2025-06-14 11:16:49 +0530

    update pom.xml

--- backend/finance-tracer-backend/src/main/java/com/financetracker/dto/UserProfileRequest.java
+++ backend/finance-tracer-backend/src/main/java/com/financetracker/dto/UserProfileRequest.java
@@ -1,5 +1,24 @@
 package com.financetracker.dto;
 
-public class UserProfileRequest {
+import lombok.Data;
 
+@Data
+public class UserProfileRequest {
+    private String fullName;
+    private String email;
+    
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
+    
 }
