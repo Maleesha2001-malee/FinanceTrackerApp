a86a459e95e0d0af238f75d79c24fb6767500c22
maleesha <139684247+Maleesha2001-malee@users.noreply.github.com>
2025-06-14 10:50:45 +0530

    add somde dto class

---
+++ backend/finance-tracer-backend/src/main/java/com/financetracker/dto/PasswordUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.financetracker.dto;
+
+import lombok.Data;
+
+@Data
+public class PasswordUpdateRequest {
+    private String currentPassword;
+    private String newPassword;
+    
+	public String getCurrentPassword() {
+		return currentPassword;
+	}
+	public void setCurrentPassword(String currentPassword) {
+		this.currentPassword = currentPassword;
+	}
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+    
+    
+}
