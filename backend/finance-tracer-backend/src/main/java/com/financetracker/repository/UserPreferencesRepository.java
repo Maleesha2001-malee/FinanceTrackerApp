fcd7160dfbc44160375c9b823ace3efd6eacf133
maleesha <139684247+Maleesha2001-malee@users.noreply.github.com>
2025-06-14 10:35:29 +0530

    UserPreferencesRepository.java added

---
+++ backend/finance-tracer-backend/src/main/java/com/financetracker/repository/UserPreferencesRepository.java
@@ -0,0 +1,12 @@
+package com.financetracker.repository;
+
+import com.financetracker.model.UserPreferences;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserPreferencesRepository extends JpaRepository<UserPreferences, Long> {
+    Optional<UserPreferences> findByUserId(Long userId);
+}
